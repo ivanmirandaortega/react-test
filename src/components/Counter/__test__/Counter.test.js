97be2941bb32d891c638bf4ce373af3a187ecb2c
ivanmirandaortega <imirandaortega.pd@gmail.com>
2022-07-22 17:43:25 -0700

    created unit test following TTD for the initial text, input and buttons

--- src/components/Counter/__test__/Counter.test.js
+++ src/components/Counter/__test__/Counter.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Counter from "../Counter";
-import { render } from "@testing-library/react";
+import { getByTestId, render } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 
 test("should render a header with correct text", () => {
@@ -8,4 +8,32 @@ test("should render a header with correct text", () => {
   const headerEl = getByTestId("header");
 
   expect(headerEl.textContent).toBe("My Counter");
+});
+
+test("counter initially start with text of 0", () => {
+  const { getByTestId } = render(<Counter />);
+  const counterEl = getByTestId("counter");
+
+  expect(counterEl.textContent).toBe("0");
+});
+
+test("input contains initial value of 1", () => {
+  const { getByTestId } = render(<Counter />);
+  const inputEl = getByTestId("input");
+
+  expect(inputEl.value).toBe("1");
+});
+
+test("add button renders with +", () => {
+  const { getByTestId } = render(<Counter />);
+  const addBtn = getByTestId("add-btn");
+
+  expect(addBtn.textContent).toBe("+");
+});
+
+test("subtract button renders with -", () => {
+  const { getByTestId } = render(<Counter />);
+  const subtractBtn = getByTestId("subtract-btn");
+
+  expect(subtractBtn.textContent).toBe("-");
 });
