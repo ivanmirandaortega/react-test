9fdee61f6a340f1a27dc1b00eb4f53fa68cbb188
ivanmirandaortega <imirandaortega.pd@gmail.com>
2022-07-27 21:29:49 -0700

    created unit tests for classname inside counter element

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -16,7 +16,14 @@ function Counter() {
   return (
     <div>
       <h1 data-testid="header">My Counter</h1>
-      <h2 data-testid="counter">{counterValue}</h2>
+      <h2
+        data-testid="counter"
+        className={`${counterValue >= 100 ? "green" : ""}${
+          counterValue <= -100 ? "red" : ""
+        }`}
+      >
+        {counterValue}
+      </h2>
       <button data-testid="subtract-btn" onClick={subtractToCounter}>
         -
       </button>
